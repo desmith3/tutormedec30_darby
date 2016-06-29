b3597e74dc2b96e584f444da07c647b920293066
desmith3 <desmith3@crimson.ua.edu>
2016-06-28 20:24:42 -0500

    uncommented line 2 through 11

--- mailInit.php
+++ mailInit.php
@@ -1,14 +1,14 @@
 
 <?php
-//require './PHPMailer-master/class.phpmailer.php';
-//require './PHPMailer-master/class.smtp.php';
+require './PHPMailer-master/class.phpmailer.php';
+require './PHPMailer-master/class.smtp.php';
 
-//$mail = new PHPMailer ();
-//$mail->IsSMTP (); // set mailer to use SMTP
-//$mail->Host = "ssl://smtp.gmail.com"; // specify main and backup server
-//$mail->Port = 465; // set the port to use
-//$mail->SMTPAuth = true; // turn on SMTP authentication
-//$mail->Username = "darbye.smith411@gmail.com"; // your SMTP username or your gmail username
+$mail = new PHPMailer ();
+$mail->IsSMTP (); // set mailer to use SMTP
+$mail->Host = "ssl://smtp.gmail.com"; // specify main and backup server
+$mail->Port = 465; // set the port to use
+$mail->SMTPAuth = true; // turn on SMTP authentication
+$mail->Username = "darbye.smith411@gmail.com"; // your SMTP username or your gmail username
                                               
 <<<<<<< HEAD
  include 'mail_password.php'; //includes my password
